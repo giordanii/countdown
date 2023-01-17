f5bc24f67604e14d24cb443af3862119fceed903
João Giordani <jgiordanii@gmail.com>
2023-01-16 23:41:43 -0300

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-const 
+const newYear = '1 jan 2024';
+
+function countdown() {
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    let days = Math.floor(totalSeconds / 3600 / 24);
+    let hours = Math.floor(totalSeconds / 3600) % 24;
+    let mins = Math.floor(totalSeconds / 60) % 60;
+    let seconds = Math.floor(totalSeconds) % 60;
+    
+
+}
